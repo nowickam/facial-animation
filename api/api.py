10711b97ba917b85f377cc984c3cde85cc63e0f1
nowickam <nowicka444@gmail.com>
2020-11-23 11:58:41 +0100

    Added file uploading to api

--- api/api.py
+++ api/api.py
@@ -1,8 +1,42 @@
-from flask import Flask
+from flask import Flask, request, abort, jsonify
+from werkzeug.utils import secure_filename
+from flask_cors import CORS, cross_origin
 import time
+import os
+import logger
+import json
 
 app = Flask(__name__)
+
+CORS(app, expose_headers='Authorization')
+
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['UPLOAD_EXTENSIONS'] = {'.wav', '.mp3', '.mp4'}
+app.config['STATIC_SOURCE'] = 'static/obama2.json'
 
 @app.route('/time')
 def get_current_time():
     return{'time': time.time()}
+
+@app.route('/upload', methods=['POST'])
+def file_upload():
+    # save the file
+    file = request.files['file'] 
+    filename = secure_filename(file.filename)
+
+    if filename != '':
+        file_ext = os.path.splitext(filename)[1]
+        if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+            abort(400)
+
+    destination="/".join([app.config['UPLOAD_FOLDER'], filename])
+    file.save(destination)
+
+    #pass the file to the ml model
+
+    # return the output
+    json_file = json.load(open(app.config['STATIC_SOURCE']))
+    return jsonify(json_file)
+
+if __name__ == "__main__":
+    app.run(debug=True)
